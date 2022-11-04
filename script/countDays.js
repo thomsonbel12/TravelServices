ae4489da9cb733ddaae4803d855327a4855bcbdf
thomsonbel12 <thomsonbel12@gmail.com>
2022-11-04 13:32:15 +0700

    dmm

--- script/countDays.js
+++ script/countDays.js
@@ -8,16 +8,27 @@ function dateFormat(inputDate) {
 
 
 function countDays(checkin_date, checkout_date){
-    let checkin = new Date(dateFormat(checkin_date));
-    let checkout = new Date(dateFormat(checkout_date));
     // console.log(checkin)
     // console.log(checkout)
-    let days = checkout.getTime() - checkin.getTime();
+    // try{
+        let checkin = new Date(dateFormat(checkin_date));
+        // console.log(checkin)
+        let checkout = new Date(dateFormat(checkout_date));
+        let days = checkout.getTime() - checkin.getTime();
 
-    return (Math.ceil(days / (1000 * 3600 * 24))) + 1;
+        if(isNaN(checkin) || isNaN(checkout)){
+            return 0;
+        }
+
+        return (Math.ceil(days / (1000 * 3600 * 24))) + 1;
+
+    // }catch{
+    //     // console.log(1)
+    //     return 0;
+    // }
 
 
 }
 
 export {dateFormat, countDays}
-// console.log(countDays("22-10-2022", "24-10-2022"))
+// console.log(countDays("22-1020", "24-0-2022"))
